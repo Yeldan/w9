671ab89c8b7ff82d4baa7cb854750990595499ce
Yeldan <yeldan99@gmail.com>
2017-05-18 17:02:48 +0600

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,3 @@
+https://oauth.vk.com/authorize?client_id=5925815&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.52
+
+ace550a3c9a06cd2003d6ee9f35713781c08f356359d5d44ef1c87eb0d07deafc04c87110a88bf38025b0
